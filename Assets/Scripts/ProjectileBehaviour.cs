0c0b35a2dd752f42fd4d67adc0a110cccdea8084
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2018-01-26 22:21:04 +1030

    Add NavMeshTest and EnemyScriptableObject

---
+++ Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour {
+
+    public ProjectileSO projectileConfig;
+
+    void Start () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = projectileConfig.speed * transform.forward;
+	}
+}
