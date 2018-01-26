7a8e1400ae858bb8e6a8b36a717477a36db382ee
Sean Mansell <thearknano@gmail.com>
2018-01-27 02:40:47 +1030

    Updated enemy ai, projectiles. Added health systems

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int StartingHealth;
+    public FloatVariable PlayerHP;
+
+    private void Start()
+    {
+        PlayerHP.Value = StartingHealth;
+    }
+
+    public void DealDamage(float damage)
+    {
+        PlayerHP.Value -= damage;
+    }
+}
