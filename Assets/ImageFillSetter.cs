bf4d0400d3c815e69f7f3d365d8ad4d9807c34e5
Sean Mansell <thearknano@gmail.com>
2018-01-27 10:15:24 +1030

    Add player effects, rearrange scripts

---
+++ Assets/ImageFillSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFillSetter : MonoBehaviour {
+
+    public float UpdateTimer;
+    public FloatVariable FloatVariable;
+    private float _updateTime;
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
+    void Update () {
+        _updateTime += Time.deltaTime;
+        if (_updateTime > UpdateTimer)
+        {
+            _updateTime = 0;
+            _image.fillAmount = FloatVariable.Value / 100;
+        }
+	}
+}
