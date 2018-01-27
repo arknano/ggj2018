c0844bf96f4fa3d86b59b2c96a63c8d98a37255c
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2018-01-27 19:32:45 +1030

    Add kill counter and portal when all enemies are killed

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour {
 
+    private KillCounter killCounter;
+
+    public void Start()
+    {
+        GameObject killCounterObject = GameObject.FindGameObjectWithTag("Kill Counter");
+        if (killCounterObject != null)
+        {
+            killCounter = killCounterObject.GetComponent<KillCounter>();
+        }
+    }
+
 	public void DealDamage(int damage)
     {
+        if (killCounter != null)
+        {
+            killCounter.EnemyKilled(gameObject);
+        }
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/KillCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillCounter : MonoBehaviour {
+
+    public GameObject levelPortal;
+
+    private List<GameObject> enemies = new List<GameObject>();
+    void Start() {
+        enemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+        levelPortal.SetActive(false);
+    }
+
+    public void EnemyKilled(GameObject gameObject)
+    {
+        if (enemies.Contains(gameObject))
+        {
+            enemies.Remove(gameObject);
+            Debug.Log("There are " + enemies.Count + " enemies");
+        }
+
+        if (enemies.Count == 0)
+        {
+            levelPortal.SetActive(true);
+        }
+    }
+}
