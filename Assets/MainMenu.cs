cdcdc66dced3eacff61cce6c8a76d24f284bfb34
Sean Mansell <thearknano@gmail.com>
2018-01-28 12:22:36 +1030

    Added new HP elements, main menu and end scenes

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public string SceneName;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent<SceneChange>().ChangeScene(SceneName);
+        }
+    }
+}
